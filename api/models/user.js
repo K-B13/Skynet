534b7317323e8b30b3adc6e9daf200e8423ffefb
Ali0k <alikore121@gmail.com>
2024-12-10 16:37:43 +0000

    user PUT test

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const { Schema, Types } = mongoose
+const { Schema } = mongoose
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  robotId: { type: Types.ObjectId, ref: "Robot"}
 });
 
 const User = mongoose.model("User", UserSchema);
