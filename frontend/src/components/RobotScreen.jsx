8e9dab26c001ebf7a3b47db2c131e559005b466c
safaa <safaa.imran7@gmail.com>
2024-12-12 13:55:55 +0000

    repair hardware button functionality

--- frontend/src/components/RobotScreen.jsx
+++ frontend/src/components/RobotScreen.jsx
@@ -1,17 +1,18 @@
-const RobotScreen = () => {
-
+const RobotScreen = (props) => {
+    console.log(props)
     return (
         <>
             <div id='robot-screen'>
                 <p id='currency'>100</p>
                 <div id='robot-stats'>
-                    <p id='battery-life'>100</p>
+                    <p id='name'>{props.name}</p>
+                    <p id='battery-life'>10</p>
                     <p id='memory-capacity'>16</p>
                     <p id='intelligence'>1</p>
                     <p id='hardware'>100</p>
                     <p id='mood'>Happy</p>
                 </div>
-                <img id='robot-image'/> 
+                <img id='robot-image' src='/neutralMood.png'/> 
             </div>
         </>
     )
