6fc94cb9c79e96d202ba5aac1a27197ee65593a5
liamquinn14 <liamq601@gmail.com>
2024-12-12 16:13:57 +0000

    add test for failed email validation in users controller

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -80,6 +80,24 @@ describe("/users", () => {
     });
   });
 
+  describe("POST, when email/password fails validation check", () => {
+    it("invalid email returns 400", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "testemail9@@test.com", password: "Something1?" });
+        expect(response.statusCode).toBe(400)
+        expect(response.body.emailErrors).toEqual(['Email must be in the correct format'])
+    })
+
+    it("invalid password returns 400", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "testemail9@test.com", password: "n0capitals!" });
+        expect(response.statusCode).toBe(400)
+        expect(response.body.passwordErrors).toEqual(['password does not have an upper case letter'])
+    })
+  })
+
   describe("PUT, updating with valid token", () => {
     it("changing email", async() => {
       const user = await User.create({
@@ -210,6 +228,27 @@ describe("/users", () => {
 
       expect(response.status).toEqual(401);
       expect(response.body.message).toEqual('auth error');
+    })
+
+    it("cannot change someone else's password", async () => {
+      const user1 = await User.create({
+        email: "userone1@example.com",
+        password: "User1password!"
+      })
+      const user2 = await User.create({
+        email: "usertwo2@example.com",
+        password: "User2password!"
+      })
+      const user1token = createToken(user1._id)
+      const response = await request(app)
+        .put(`/users/${user2._id}`)
+        .set("Authorization", `Bearer ${user1token}`)
+        .send({ password: "User1password!" });
+      const users = await User.find();
+      expect(users[0].email).toEqual("userone1@example.com");
+      expect(users[0].password).toEqual("User1password!");
+      expect(users[1].email).toEqual("usertwo2@example.com");
+      expect(users[1].password).toEqual("User2password!");
     })
   })
 
