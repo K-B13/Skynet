e2d3c3c939f255c5e04d0e130078555d6036d334
safaa <safaa.imran7@gmail.com>
2024-12-12 11:42:37 +0000

    landing page and updating login page

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,19 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getPayloadFromToken } from "../../helpfulFunctions/helpfulFunctions";
 import RobotScreen from "../../components/RobotScreen"
 import MemoryButtons from "../../components/MemoryButtons"
 import RepairButton from "../../components/RepairButton"
 import SpeakButton from "../../components/SpeakButton"
 import EnergyButtons from "../../components/EnergyButtons"
+import { getRobotByUserId } from "../../services/robot";
 
 const LandingPage = () => {
 
+    const [robotData, setRobotData] = useState([]);
+
     useEffect(() => {
-        const async robot = await fetch(getRobotData);
+        const fetchRobot = async() => {
+            const token = localStorage.getItem("token");
+            const user = getPayloadFromToken(token);
+            try {
+                const robot = await getRobotByUserId(user.userId);
+                setRobotData(robot);
+            } catch (err) {
+                console.error("error fetching user robot", err);
+            }
+        }
+        fetchRobot();
     }, []);
 
     return (
         <>
-        <RobotScreen/>
+        <RobotScreen 
+            currency={robotData.currency}
+            batteryLife={robotData.batteryLife}
+            memoryCapacity={robotData.memoryCapacity}
+            intelligence={robotData.intelligence}
+            hardware={robotData.hardware}
+            mood={robotData.mood}
+            img={robotData.img}/>
         <EnergyButtons/>
         <MemoryButtons/>
         <RepairButton/>
