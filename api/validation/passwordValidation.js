a3f1a30ca0ae6c30e50add0fc191872844562a7c
liamquinn14 <liamq601@gmail.com>
2024-12-12 11:25:25 +0000

    add password validation unit tests to increase coverage

--- api/validation/passwordValidation.js
+++ api/validation/passwordValidation.js
@@ -1,87 +1,93 @@
 const passwordMinLength = (password) => {
-    if (password.length >= 8) return { passes: true }
-    return { passes: false, message: 'password is too short' }
-}
+    if (password.length >= 8) return { passes: true };
+    return { passes: false, message: "password is too short" };
+};
 
 const passwordMaxLength = (password) => {
-    if (password.length <= 20) return { passes: true }
-    return { passes: false, message: 'password is too long' }
-}
+    if (password.length <= 20) return { passes: true };
+    return { passes: false, message: "password is too long" };
+};
 
 const passwordSpecialCharacter = (password) => {
     const specialCharacterRegex = /[^a-zA-Z0-9\s]/;
-    if (specialCharacterRegex.test(password)) return { passes: true }
-    return { passes: false, message: 'password does not have special character' }
-}
+    if (specialCharacterRegex.test(password)) return { passes: true };
+    return { passes: false, message: "password does not have special character" };
+};
 
 const passwordHasAnInteger = (password) => {
     const integerRegex = /\d/;
-    if (integerRegex.test(password)) return { passes: true }
-    return { passes: false, message: 'password does not have an integer' }
-}
+    if (integerRegex.test(password)) return { passes: true };
+    return { passes: false, message: "password does not have an integer" };
+};
 
 const passwordIncludesUpperCase = (password) => {
-    const uppercaseRegex = /[A-Z]/
-    if (uppercaseRegex.test(password)) return { passes: true }
-    return { passes: false, message: 'password does not have an upper case letter' }
-}
+    const uppercaseRegex = /[A-Z]/;
+    if (uppercaseRegex.test(password)) return { passes: true };
+    return {
+    passes: false,
+    message: "password does not have an upper case letter",
+    };
+};
 
 const passwordIncludesLowerCase = (password) => {
-    const lowercaseRegex = /[a-z]/
-    if (lowercaseRegex.test(password)) return { passes: true }
-    return { passes: false, message: 'password does not have a lower case letter' }
-}
+    const lowercaseRegex = /[a-z]/;
+    if (lowercaseRegex.test(password)) return { passes: true };
+    return {
+    passes: false,
+    message: "password does not have a lower case letter",
+    };
+};
 
 const passwordIsString = (password) => {
-    if (typeof password === 'string') return { passes: true }
-    return { passes: false, message: 'password must be a string' }
-}
+    if (typeof password === "string") return { passes: true };
+    return { passes: false, message: "password must be a string" };
+};
 
 const passwordNoTrailingSpaces = (password) => {
-    if (password[0] === ' ' || password[password.length - 1] === ' ') return { passes: false, message: 'password must not have spaces on either end of the password' }
-    return { passes: true }
-}
+    if (password[0] === " " || password[password.length - 1] === " ")
+    return {
+        passes: false,
+        message: "password must not have spaces on either end of the password",
+    };
+    return { passes: true };
+};
 
 const allPasswordChecks = (password) => {
-    let errorMessages = []
-    if (!password) {
-        errorMessages.push('Invalid password type')
-        return errorMessages
+    let errorMessages = [];
+    if (!password || !passwordIsString(password).passes) {
+    errorMessages.push("Invalid password type");
+    return errorMessages;
     }
-    let result = passwordMinLength(password)
+    result = passwordMinLength(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordMaxLength(password)
+    result = passwordMaxLength(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordIncludesUpperCase(password)
+    result = passwordIncludesUpperCase(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordIncludesLowerCase(password)
+    result = passwordIncludesLowerCase(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordSpecialCharacter(password)
+    result = passwordSpecialCharacter(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordHasAnInteger(password)
+    result = passwordHasAnInteger(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordIsString(password)
+    result = passwordNoTrailingSpaces(password);
     if (!result.passes) {
-        errorMessages.push(result.message)
+    errorMessages.push(result.message);
     }
-    result = passwordNoTrailingSpaces(password)
-    if (!result.passes) {
-        errorMessages.push(result.message)
-    }
-    return errorMessages
-}
+    return errorMessages;
+};
 
 const passwordValidation = {
     passwordMinLength: passwordMinLength,
@@ -92,7 +98,7 @@ const passwordValidation = {
     passwordIncludesLowerCase: passwordIncludesLowerCase,
     passwordIsString: passwordIsString,
     passwordNoTrailingSpaces: passwordNoTrailingSpaces,
-    allPasswordChecks: allPasswordChecks
-}
+    allPasswordChecks: allPasswordChecks,
+};
 
-module.exports = passwordValidation
+module.exports = passwordValidation;
