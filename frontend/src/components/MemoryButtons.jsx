c5d1a58d69fca1c394b2188929e602495dc0e1c2
Kelly <kellyhowes83@gmail.com>
2024-12-12 15:33:38 +0000

    Memory buttons functionality created

--- frontend/src/components/MemoryButtons.jsx
+++ frontend/src/components/MemoryButtons.jsx
@@ -1,13 +1,37 @@
-import { useEffect } from "react";
+import {updateRobotMemory, updateRobotIntelligence} from "../services/robot"
 
+const MemoryButtons = (props) => {
+    const handleTeach = async () => {
+        try {
+            const response = await updateRobotIntelligence(props.robotId, 5);
+            if(response.message === "Robot intelligence increased"){
+                
+                props.setRobotData(response.robot);
+            }
+            if(response.message === "Robot intelligence did not increase"){
+                props.setDidNotLearn(true)
+            }
+        } catch (err) {
+            console.error("error updating robot intelligence", err);
+        }
+    }
 
-const MemoryButtons = () => {
+    const handleUpgrade = async () => {
+        try {
+            const response = await updateRobotMemory(props.robotId);
+            if(response.message === "Robot memory upgraded"){
+                props.setRobotData(response.robot);
+            }
+        } catch (err) {
+            console.error("error upgrading robot Memory", err);
+        }
+    }
 
     return (
         <>
             <div id='memory-buttons'>
-                <button id='teach-button'>teach</button>
-                <button id='upgrade-button'>upgrade memory</button>
+                <button id='teach-button' onClick={handleTeach}>teach</button>
+                <button id='upgrade-button' onClick={handleUpgrade}>upgrade memory</button>
             </div>
         </>
     )
