1346981a4e329f4162a0348cb7e0dd3592d6d689
Kelly <kellyhowes83@gmail.com>
2024-12-15 15:41:34 +0000

    Changed some styling/ conditional rendering of rules

--- frontend/src/pages/VirusSweeper/VirusSweeperPage.jsx
+++ frontend/src/pages/VirusSweeper/VirusSweeperPage.jsx
@@ -7,6 +7,7 @@ import { getPayloadFromToken } from "../../helpfulFunctions/helpfulFunctions";
 const VirusSweeperPage = () => {
 const mineImage = '/assets/virus.png'; 
 const [robotData, setRobotData] = useState({});
+const [gameStarted, setGameStarted] = useState(false);
 
 useEffect(() => {
     const fetchRobot = async() => {
@@ -24,10 +25,41 @@ useEffect(() => {
     fetchRobot();
 }, []);
 
+const handleStartGame = () => {
+    setGameStarted(true);
+};
+
     return (
-    <div id="minesweeper-page">
+    <div id="virus-sweeper-page">
+
         <h1>Virus Sweeper</h1>
-        <VirusSweeperBoard robotId={robotData._id}rows={10} cols={10} mineCount={20} mineImage={mineImage} />
+        {!gameStarted ? (
+            <div id="virus-sweeper-rules">
+                    <h2>Rules</h2>
+                    <p>The goal is to reveal all the cells that do not contain a virus.
+                    <br />
+                    <br />
+                    Each revealed cell shows a number indicating how many adjacent cells contain mines. 
+                    <br />
+                    <br />
+                    Cells with no adjacent mines are revealed automatically.
+                    <br />
+                    <br/>
+                    The game is won when all non-virus cells are revealed.
+                    <br />
+                    <br />
+                    You will score a point for every safe cell you reveal. 1 point is equal to $1
+                    </p>
+        
+                    <h3 id="virus-sweeper-controls">Controls</h3>
+                    <p>Use your mouse to click on a cell to reveal it. If the cell contains a virus, the game is over.</p>
+                <button onClick={handleStartGame} id="virus-sweeper-start-button">
+                    Start Game
+                </button>
+                </div>
+            ) : (
+                <VirusSweeperBoard robotId={robotData._id}rows={10} cols={10} mineCount={20} mineImage={mineImage} />
+            )}
     </div>
     );
 };
