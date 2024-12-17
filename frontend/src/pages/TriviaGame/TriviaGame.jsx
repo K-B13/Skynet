a11c89f8af08ead3d544090e47bbcaa7a76fd203
Kelly <kellyhowes83@gmail.com>
2024-12-17 15:41:52 +0000

    forgot to put questions back to 20 instead of 2

--- frontend/src/pages/TriviaGame/TriviaGame.jsx
+++ frontend/src/pages/TriviaGame/TriviaGame.jsx
@@ -27,7 +27,7 @@ export function TriviaGame() {
             setLoading(true);
             try {
                 const response = await fetch(
-                    `https://opentdb.com/api.php?amount=2&difficulty=${level}&type=multiple`
+                    `https://opentdb.com/api.php?amount=20&difficulty=${level}&type=multiple`
                 );
                 const data = await response.json();
                 if (data.results) {
