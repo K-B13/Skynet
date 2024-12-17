c6cb4fad9c0ab241b02e0b4dd2322cb470973a24
Kelly <kellyhowes83@gmail.com>
2024-12-17 15:40:07 +0000

    trivia game updates funds

--- frontend/src/pages/TriviaGame/TriviaGame.jsx
+++ frontend/src/pages/TriviaGame/TriviaGame.jsx
@@ -27,7 +27,7 @@ export function TriviaGame() {
             setLoading(true);
             try {
                 const response = await fetch(
-                    `https://opentdb.com/api.php?amount=20&difficulty=${level}&type=multiple`
+                    `https://opentdb.com/api.php?amount=2&difficulty=${level}&type=multiple`
                 );
                 const data = await response.json();
                 if (data.results) {
@@ -48,11 +48,12 @@ export function TriviaGame() {
 
     useEffect(() => {
         if (gameOver) {
-            calculateCash(level, score);
+            const cash = calculateCash(level, score);
+            setMoneyEarned(cash)
+            
             const updateCurrency = async () => {
                 try {
-                    const response = await updateRobotCurrency(robotId, moneyEarned);
-                    console.log("MONEY EARNED TRIVIA: ",moneyEarned);
+                    const response = await updateRobotCurrency(robotId, cash);
                     
                     if(response.message === 'robot currency updated'){
                         navigate('/landingpage')
@@ -78,11 +79,11 @@ export function TriviaGame() {
 
     const calculateCash = (level, score) => {
         if (level === "easy") {
-            setMoneyEarned(score * 5);
+            return score * 5
         } else if (level === "medium") {
-            setMoneyEarned(score * 10);
+            return score * 10
         } else {
-            setMoneyEarned(score * 15);
+            return score * 15
         }
     };
 
