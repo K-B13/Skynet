a718563f66641e0706ca041cf5ac2fab133a6085
safaa <safaa.imran7@gmail.com>
2024-12-19 09:07:33 +0000

    trivia button selection

--- frontend/src/pages/TriviaGame/TriviaGamePage.jsx
+++ frontend/src/pages/TriviaGame/TriviaGamePage.jsx
@@ -7,12 +7,29 @@ import NavBar from "../NavBar/NavBar";
 export function TriviaGamePage() {
     const location = useLocation()
     const { robotId } = location.state || '';
+    const [easyButtonClicked, setEasyButtonClicked] = useState(false)
+    const [mediumButtonClicked, setMediumButtonClicked] = useState(false)
+    const [hardButtonClicked, setHardButtonClicked] = useState(false)
     
     const [level, setLevel] = useState('')
     const navigate = useNavigate()
 
     const handleSelection = (e) =>{
         setLevel(e.target.value)
+        if(e.target.value === 'easy'){
+            setEasyButtonClicked(true)
+            setMediumButtonClicked(false) 
+            setHardButtonClicked(false) 
+        } else if(e.target.value === 'medium'){
+            setEasyButtonClicked(false)
+            setMediumButtonClicked(true)
+            setHardButtonClicked(false) 
+        } else if(e.target.value === 'hard'){
+            setEasyButtonClicked(false)
+            setMediumButtonClicked(false) 
+            setHardButtonClicked(true) 
+        }
+
     }
     
     const handleStart = () =>{
@@ -24,7 +41,17 @@ export function TriviaGamePage() {
         }
     }
     
-    
+    // const handleClick = () => {
+    //     const buttons = document.querySelectorAll('.difficulty-buttons');
+    //     buttons.forEach(button => {
+    //         button.addEventListener('click', () => {
+    //             document.querySelector('.clicked')?.classList.remove('clicked');
+    //             button.classList.add('clicked');
+    //         });
+    //     });        
+    // }
+
+
     return (
     <div id="trivia-game-rules-page">
         <NavBar robotId={robotId}/>
@@ -41,9 +68,9 @@ export function TriviaGamePage() {
             Hard level will give you $15 x your final score
         </p>
         <h3> Please select your difficulty level</h3>
-        <button value="easy"onClick={handleSelection}>Easy</button>
-        <button value="medium"onClick={handleSelection}>Medium</button>
-        <button value="hard"onClick={handleSelection}>Hard</button>
+        <button className={easyButtonClicked ? 'clicked' : ''} value="easy"onClick={handleSelection}>Easy</button>
+        <button className={mediumButtonClicked ? 'clicked' : ''} value="medium"onClick={handleSelection}>Medium</button>
+        <button className={hardButtonClicked ? 'clicked' : ''} value="hard"onClick={handleSelection}>Hard</button>
         <p>Click start to start the game</p>
         <button onClick={handleStart}>Start</button>
         </div>
